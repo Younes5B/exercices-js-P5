17b189c78ef6a53a72ca83791e5dd3567ba05950
Yonas PC2 <younesbeji55@gmail.com>
2025-08-22 11:09:58 +0200

    exo 7 8 9 10

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,20 @@
 //votre code ici
 
 export default printNumbers
+
+function printNumbers(n) {
+  let x = '';
+
+  for (let i = 1; i <= n; i++) {
+    x += i;
+    if (i < n) {
+      x += ' ';
+    }
+  }
+
+  return x;
+}
+
+console.log(printNumbers(8));
+
+export default printNumbers;
--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,9 @@
 //Votre code ici
 
  export default add;
+
+ function add (nombre1, nombre2) {
+   return nombre1 + nombre2;
+ }
+
+console.log(add(5,13));
